c315b653799c1843b2ac036072ec60db59f739d9
Yensey Murillo <yenseymurillo@gmail.com>
2020-04-19 16:28:02 -0500

    changed the onclick to the app.js instead of card.js

--- clicky-react-app/app/src/App.js
+++ clicky-react-app/app/src/App.js
@@ -1,19 +1,128 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
 import Navbar from "./components/navbar";
 import Jumbotron from "./components/jumbotron";
+import images from "./cardimage.json";
 import Card from "./components/card";
-import Score from "./components/Score";
+import "./App.css";
+// import Score from "./components/Score";
 
-function App() {
-  return (
-    <div className="App">
-        <Navbar />
-        <Jumbotron />
-        <br></br>
-        <Score />
-    </div>
-  );
+let score = 0;
+let topScore = 0;
+let message = "Click Any Image to begin!";
+
+
+class App extends Component {
+ 
+  state ={
+    images,
+    message,
+    score,
+    topScore
+  };
+
+  cardClick = id => {
+  
+      const clickedImg = images.filter(match => match.id === id);
+      
+      if (clickedImg[0].clicked){
+
+          score = 0;
+          message = "You've clicked me already!";
+
+          for (let i = 0; i < images.length ; i++){
+            images[i].clicked = false;
+          }
+
+          this.setState({message});
+          this.setState({score});
+          this.setState({images});
+
+        } else if (score < 11){
+
+          clickedImg[0].clicked = true;
+          score++;
+          message = "Great job! Keep Going!!"
+
+          if(score > topScore){
+            topScore = score;
+            this.setState({ topScore });
+          }
+
+          images.sort(function(a, b){return 0.5 - Math.random()});
+
+          this.setState({ images });
+          this.setState({ score });
+          this.setState({ message });
+
+        } else {
+
+          clickedImg[0].clicked = true;
+
+          score = 0;
+          message = "Congrats! You got a memory of an elephant!";
+          topScore = 12;
+          this.setState({ topScore });
+
+          for(let i = 0; i< images.length; i ++){
+              images[i].clicked = false;
+          }
+
+          images.sort(function(a, b){return 0.5 -Math.random()});
+
+          this.setState({ images });
+          this.setState({ score });
+          this.setState({ message });
+
+        }
+  
+  };
+
+  render() {
+
+    return (
+      <div>
+       <Navbar />
+       <Jumbotron /> 
+      <div className="container">
+          <div class="row">
+            {this.state.images.map(image => (
+          <div onClick={() => this.cardClick(image.id)} className="card col-md-3">
+              <Card
+                key={image.id}
+                id={image.id}
+                name={image.name}
+                clicked={image.clicked}
+                image={image.image}
+                handleClick={this.handleClick}
+                />
+          </div>
+            ))}
+          </div>
+      </div>
+
+      <nav class="navbar fixed-bottom navbar-light bg-light">
+        <p class="score"> Score: {this.state.score} | Top Score: {this.state.topScore}</p>
+          <p>{this.state.message}</p>
+      </nav>
+
+      </div>
+     
+
+       
+    );
+
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//         <Navbar />
+//         <Jumbotron />
+//         <br></br>
+//         <Score />
+//     </div>
+//   );
+// }
 
 export default App;
--- clicky-react-app/app/src/components/card.js
+++ clicky-react-app/app/src/components/card.js
@@ -3,13 +3,16 @@ import "../components/card.css";
 
 const Card = props => (
  
-    <div class="card col-md-3" key={props.id} onClick={() => props.cardClick(props.id, props.clicked)}>
-      <div class="card-body">
-      <img id={props.name} src={props.image} class="card-img-top" alt={props.name} />
+    <div >
+      <div className="card-body">
+          <img className="card-img-top" 
+            src={props.image} 
+            alt={props.name} 
+            id={props.id}
+          />
        </div>
     </div>
 
   );
-
 
 export default Card;
