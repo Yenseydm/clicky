ee551221623c9301ef6cc56ec5a7a891e95a39c0
Yensey Murillo <yenseymurillo@gmail.com>
2020-04-21 22:08:35 -0500

    Updates

---
+++ precache-manifest.7389face45eac94fff68fc2f2ebae986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a6eceefb5dd836ceb72ecc9950f510",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "7dfc42a8bbf67efb4d6c",
+    "url": "/clicky/static/css/main.1c74fdad.chunk.css"
+  },
+  {
+    "revision": "9f2beb5a0d22d8f3ca12",
+    "url": "/clicky/static/js/2.f8268704.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky/static/js/2.f8268704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dfc42a8bbf67efb4d6c",
+    "url": "/clicky/static/js/main.89fb5b56.chunk.js"
+  },
+  {
+    "revision": "335dc96dc2a4e265efcf",
+    "url": "/clicky/static/js/runtime-main.2018f1d6.js"
+  }
+]);
